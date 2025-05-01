59e8eb3a9424ec17361faa554bfef615b6d54052
Be-A-Coder01 <adeshmendekar07@gmail.com>
2025-05-01 20:43:05 +0530

    10th  commit

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -4,16 +4,16 @@ import rit from "../assets/rit.png";
 
 const Navbar = () => {
   return (
-    <div className=" z-20 bg-opacity-30 backdrop-blur-lg backdrop-filter fixed top-0 flex right-0 left-0 justify-between py-3 md:py-5 text-center text-white bg-transparent place-items-center">
+    <div className=" z-20 bg-opacity-30 backdrop-blur-lg backdrop-filter fixed gap-y-3.5 top-0 flex flex-col md:flex-row right-0 left-0 justify-between py-3 md:py-5 text-center text-white bg-transparent place-items-center">
       <div>
         <p className="text-[10px] ml-5 md:h-[20px] md:font-bold md:ml-11 ">
-          <img src={rit} className="border-2  h-[30px] md:h-[45px]" />
+          <img src={rit} className="  h-[30px] md:h-[45px]" />
         </p>
       </div>
       <div>
-        <ul className="flex text-[12px] py-3 gap-3 mr-4 md:text-lg md:font-semibold md:gap-10 md:mr-10 ">
+        <ul className="flex text-[12px] py-3 gap-6 mr-4 md:text-lg md:font-semibold md:gap-10 md:mr-10 ">
           <Link to="/">Home</Link>
-          {/* <li>Events</li> */}
+          <li>About</li>
           <Link to="/events">Events</Link>
           <Link to="/members">Team</Link>
           <li>Contact</li>
