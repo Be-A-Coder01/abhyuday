75b55295e77a54f6c89d1fdf509307bbd73129bd
Be-A-Coder01 <adeshmendekar07@gmail.com>
2025-05-03 20:34:32 +0530

    17th  commit

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -16,8 +16,8 @@ const About = () => {
         <div id="stars3" className="-z-10"></div>
       </div>
       <Navbar></Navbar>
-      <div className=" mt-[300px]  md:mt-14  flex justify-center place-items-center h-screen">
-        <div className="text-white leading-5 tracking-widest md:text-center bg-[#0F1016] p-10 rounded-lg text-xs  md:tracking-widest md:text-xl md:leading-10 w-[90vw] ">
+      <div className=" mt-[200px]  md:mt-14  flex justify-center place-items-center h-screen">
+        <div className="text-white leading-5 tracking-widest md:text-center bg-[#0F1016] px-5 py-10 md:p-10 rounded-lg text-xs  md:tracking-widest md:text-xl md:leading-10 w-[90vw] ">
           About <span className="text-pink-400 font-bold">Abhyuday 9.0</span> .
           The Department of Master of Computer Applications at{" "}
           <span className="text-pink-400 font-bold">
