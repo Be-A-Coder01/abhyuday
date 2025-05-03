8b540b0a63c954125707413b8fddae7e4306c721
Be-A-Coder01 <adeshmendekar07@gmail.com>
2025-05-03 19:25:08 +0530

    14th  commit

--- client/src/Components/Team.jsx
+++ client/src/Components/Team.jsx
@@ -13,6 +13,7 @@ import jeevitha from "../assets/jeevitha.jpeg";
 import sriram from "../assets/sriram.jpeg";
 import prithvi from "../assets/prithvi.jpeg";
 import aditya from "../assets/aditya.jpg";
+import blen from "../assets/blen.jpeg";
 
 const Team = () => {
   return (
@@ -152,6 +153,29 @@ const Team = () => {
             <p className="text-pink-400 text-lg font-semibold ">8762001156</p>
           </div>
 
+          {/* Description */}
+        </div>
+
+        <div className="w-[460px] z-10 h-[350px]  bg-[#1E1E2D] rounded-xl p-6  flex flex-col items-center text-center shadow-lg">
+          {/* Profile Image */}
+          <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-pink-500 mb-4">
+            <img
+              src={blen} // <-- Replace this with your image path
+              alt="Blen"
+              className="object-cover w-full h-full"
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-white text-2xl  font-bold">Blen</h2>
+            <p className=" text-white tag text-md rounded-md py-1 px-2 font-semibold bg-gradient-to-r from-pink-500/30 to-purple-500/30">
+              Technical Support
+            </p>
+
+            {/* Name */}
+
+            {/* Role */}
+            <p className="text-pink-400 text-lg font-semibold ">9606027172</p>
+          </div>
           {/* Description */}
         </div>
 
